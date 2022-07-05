7708e7fc78548fef556ccaf7485cc93b99740fa9
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-07-04 23:06:01 -0600

    Add zip and unzip in shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.buildPackages.love ];
+  nativeBuildInputs = [ pkgs.buildPackages.love pkgs.zip pkgs.unzip ];
 }
