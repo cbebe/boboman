c2673e5790ccbee8dc773726773ccc3cece4d43f
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-07-06 00:19:20 -0600

    Deploy Love.js - based on b718b11485f9a543e933e01a5c0474463f375aed

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"bcd6f1c0-4b6e-4969-87c4-7da08f5edf81","remote_package_size":13091,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":13091,"audio":false}]});
+  loadPackage({"package_uuid":"51b7ff95-6f44-4564-b7a5-221fb67e0b57","remote_package_size":13291,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":13291,"audio":false}]});
 
 })();
